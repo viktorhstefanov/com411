7f1f953eb8f7a56a3722fbd51e8c7897b4115b80
Viktor Stefanov <2stefv98@solent.ac.uk>
2023-09-28 12:50:14 +0100

    input2

--- week1/input_tasks.py
+++ week1/input_tasks.py
@@ -3,11 +3,13 @@
 #name = input()
 #print (f"It is nice to meet you {name}")
 
-print("Please enter a character for the eye")
-eye = input()
-print("##########")
-print(f"# {eye}    {eye} #")
-print("#        #")
-print("##########")
+#print("Please enter a character for the eye")
+#eye = input()
+#print("##########")
+#print(f"# {eye}    {eye} #")
+#print("#        #")
+#print("##########")
+
+print("What is your name")
 
 
