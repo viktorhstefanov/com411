7e7e529157fb6bb5a3eee76c0875d2adc9e9cdfb
Viktor Stefanov <2stefv98@solent.ac.uk>
2023-10-06 10:05:58 +0100

    Added code to demonstrate the use of comparison operators.

--- week2/if_statements.py
+++ week2/if_statements.py
@@ -41,3 +41,17 @@ if remainder != 0:
     print(f"The number {number} is an odd number.")
 elif remainder == 0:
     print(f"The number {number} is an even number.")
+
+
+
+print("Please enter the first number.")
+first_number = int(input())
+print("Please enter the second number.")
+second_number = int(input())
+
+if first_number < second_number:
+    print("The first number is the smallest!")
+elif second_number < first_number:
+    print("The second number is the smallest!")
+elif first_number == second_number:
+    print("Both numbers are equal!")
