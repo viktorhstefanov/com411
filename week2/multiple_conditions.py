55dce7f33603abe4b7ad7248f9a664334f184e0e
viktorhstefanov <2stefv98@solent.ac.uk>
2023-10-06 16:25:12 +0100

    Added code to demonstrate the use of a logical and operator.

--- week2/multiple_conditions.py
+++ week2/multiple_conditions.py
@@ -9,3 +9,14 @@ elif (type == "safe") or (type == "long"):
 else:
     print("Not sure which route to take.")
 
+print("What did I hear?")
+sound = input()
+print("What did I see?")
+object = input()
+
+if (sound == "grrr") and (object == "two red eyes"):
+    print("There is a scary creature. I should get out of here!")
+else:
+    print("I am a little scared but I will continue!")
+
+
