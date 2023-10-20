ffacf1d493b9fde2f0d949669730126ad7a54c6b
Viktor Stefanov <2stefv98@solent.ac.uk>
2023-10-20 09:47:32 +0100

    Added code to demonstrate the use of built-in function:abs,range,chr.

--- week4/built_in_functions.py
+++ week4/built_in_functions.py
@@ -1,9 +1,18 @@
 # Activity 1:
 print("Program started!")
 print("Please enter a standard character:")
-char = input()
-if len(char) == 1:
-    value = ord(char)
-print(f"The ASCII code for {char} is {value}.")
+word = input()
+if len(word) == 1:
+    value = ord(word)
+print(f"The ASCII code for {word} is {value}.")
 print("Program Ended!")
 
+# Activity 2
+print("Program Started!")
+print("Please enter an ASCII code:")
+code = int(input())
+
+if code in range(32, 126):
+    char = chr(code)
+print(f"The character represented by the ASCII {code} is {char}.")
+print("Program Ended!")
