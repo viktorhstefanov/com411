c7e1099bc00e6f828b19089e36c3fb38c219348f
Viktor Stefanov <2stefv98@solent.ac.uk>
2023-10-20 09:35:33 +0100

    Added code to demonstrate the use of built-in functions:len,ord.

---
+++ week4/built_in_functions.py
@@ -0,0 +1,9 @@
+# Activity 1:
+print("Program started!")
+print("Please enter a standard character:")
+char = input()
+if len(char) == 1:
+    value = ord(char)
+print(f"The ASCII code for {char} is {value}.")
+print("Program Ended!")
+
