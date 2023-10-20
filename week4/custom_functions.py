4d74c26361735d72009d494fe80c8d17b7b9bc55
Viktor Stefanov <2stefv98@solent.ac.uk>
2023-10-20 09:58:00 +0100

    Added code to demonstrate the use of a user-defined function with a nested decision.

--- week4/custom_functions.py
+++ week4/custom_functions.py
@@ -6,3 +6,16 @@ def listen():
 
 
 listen()
+
+# Activity 2
+def identify():
+    print("What lies before us?")
+    object = input()
+    if object == "a large boulder":
+        print("It's time to run!")
+    else:
+        print("We will be fine.")
+
+
+identify()
+
