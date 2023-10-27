e2671f3a84b7d3f6b75dba9b9034c5a86dc661e5
Viktor Stefanov <2stefv98@solent.ac.uk>
2023-10-27 09:27:33 +0100

    Added code to demonstrate a single code.

---
+++ week5/list_tasks.py
@@ -0,0 +1,14 @@
+def directions():
+    steps = []
+    steps.append("Move Forward")
+    steps.append("Move Backward")
+    steps.append("Turn Left")
+    steps.append("Turn Right")
+    print(steps)
+
+
+def run_task1():
+    directions()
+
+
+run_task1()
