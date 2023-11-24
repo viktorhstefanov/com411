db007a2eafcd62cea189b2d768a60ba36c88aaef
Viktor Stefanov <2stefv98@solent.ac.uk>
2023-11-24 10:01:42 +0000

    Updated classes human and robot with instance methods.

--- week9/robot.py
+++ week9/robot.py
@@ -16,6 +16,30 @@ class Robot:
     def __repr__(self):
         return f'robot(name={self.name}, age={self.age}, energy={self.energy})'
 
+    def grow(self):
+        self.age += 1
+
+    def eat(self,amount):
+        replenish = self.energy + amount
+        if replenish <= 100:
+            self.energy = replenish
+        elif replenish > 100:
+            self.energy = 100
+
+    def move(self, distance):
+        movement = self.energy - distance
+        if movement <= 0:
+            self.energy = 0
+        elif movement >= 0:
+            self.energy = movement
+
+
+
+
+
+
+
+
 
 robot = Robot()
 robot.display()
