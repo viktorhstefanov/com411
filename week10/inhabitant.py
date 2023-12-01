1c778699ca1c90b871cde95930fb2e3173f43a07
Viktor Stefanov <2stefv98@solent.ac.uk>
2023-12-01 10:46:18 +0000

    Added code to Planet class.

--- week10/inhabitant.py
+++ week10/inhabitant.py
@@ -19,4 +19,21 @@ class Inhabitant():
        Energy: {self.energy}
 """)
 
+    def grow(self):
+        self.age += 1
+
+    def eat(self, amount):
+        replenish = self.energy + amount
+        if replenish <= 100:
+            self.energy = replenish
+        elif replenish > 100:
+            self.energy = 100
+
+    def move(self, distance):
+        movement = self.energy - distance
+        if movement <= 0:
+            self.energy = 0
+        elif movement >= 0:
+            self.energy = movement
+
 
